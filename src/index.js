296e9bad224c9b6202f07ae46e43373d18cdd284
Timothy Shores <timothymshores@gmail.com>
2019-02-13 17:56:54 -0600

    Created TodoItems.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.css"
+import TodoList from "./TodoList";
 
 var destination = document.querySelector("#container");
 
 ReactDOM.render(
     <div>
-        <p>Hello</p>
+        <TodoList />
     </div>,
     destination
 );
