d182b65f344da76a761e747381d8ade2381725c2
Timothy Shores <timothymshores@gmail.com>
2019-02-14 17:18:12 -0600

    Added TodoList.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -6,7 +6,17 @@ import React from 'react';
 import Todo from './Todo';
 
 const TodoList = props => {
-    return ('To Do List');
+    return (
+        <div>
+            {props.todos.map(todo => (
+                <Todo
+                    handleToggleComplete={props.handleToggleComplete}
+                    key={todo.id}
+                    todo={todo}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default TodoList;
