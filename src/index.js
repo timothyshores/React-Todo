40763115cf289cfd4681137e90dbd594ccd7ab42
Timothy Shores <timothymshores@gmail.com>
2019-02-14 17:09:53 -0600

    Removed tutorial files in components folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
-import "./index.css"
-import TodoList from "./TodoList";
 
-var destination = document.querySelector("#container");
-
-ReactDOM.render(
-    <div>
-        <TodoList />
-    </div>,
-    destination
-);
+render(<App />, document.getElementById('root'));
